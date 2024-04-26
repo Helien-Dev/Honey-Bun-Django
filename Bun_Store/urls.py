60afc35911f5d263d35a8a0fd3f058008eb9a460
Helien-Dev <alvaroaviladev@gmail.com>
2024-04-26 18:57:41 -0500

    User - login and signup added

--- Bun_Store/urls.py
+++ Bun_Store/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bun_Home, name="home")
+    path('', views.bun_Home, name="home"),
+    path('login/', views.user_login, name="user_login"),
+    path('signup/', views.user_signup, name="user_signup"),
+    path('logout/', views.user_logout, name="user_logout"),
 ]
+
--- Bun_Store/views.py
+++ Bun_Store/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 def bun_Home(request):
-    context = {"title": "Honey Bun Shop", "description": "Home page of Honey Bun Shop"}
+    username = request.user
+    context = {
+        "title": "Honey Bun Shop",
+        "description": "Home page of Honey Bun Shop",
+        "username": username,
+    }
     return render(request, "bun_home.html", context)
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            error_message = "Invalid username or password"
+            return render(request, "user_login.html", {"error_message": error_message})
+
+    return render(request, "user_login.html")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repeatPassword = request.POST["repeatPassword"]
+
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return redirect("/")
+            except:
+                error_message = "Error creating account"
+                return render(
+                    request, "user_signup.html", {"error_message": error_message}
+                )
+        else:
+            error_message = "Password do not match"
+            return render(request, "user_signup.html", {"error_message": error_message})
+    return render(request, "user_signup.html")
+
+
+def user_logout(request):
+    pass
