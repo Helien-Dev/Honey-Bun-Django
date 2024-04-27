10c33c5ec992bd34bf17890ed2baa6cf2a876c80
Helien-Dev <alvaroaviladev@gmail.com>
2024-04-27 15:10:42 -0500

    list admin fixed

--- Bun_Store/admin.py
+++ Bun_Store/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'last_name', 'email')  
+admin.site.register(Customer, CustomerAdmin)
--- Bun_Store/models.py
+++ Bun_Store/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=200, null=True)
     last_name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=300, null=True)
+    name = models.CharField(max_length=300, null=True)
 
     @receiver(post_save, sender=User)
     def create_or_update_customer(sender, instance, created, **kwargs):
@@ -24,6 +25,7 @@ class Customer(models.Model):
                     email=instance.email,
                     first_name=instance.first_name,
                     last_name=instance.last_name,
+                    name=instance.name
                 )
             except Exception as e:
                 print(f"Error trying to create customer: {e}")
@@ -32,12 +34,13 @@ class Customer(models.Model):
                 instance.customer.email = instance.email 
                 instance.customer.first_name = instance.first_name
                 instance.customer.last_name = instance.last_name
+                instance.customer.name = f"{instance.first_name} {instance.last_name}" 
                 instance.customer.save()
             except Exception as e:
                 print(f"Error trying to update customer: {e}")
 
     def __str__(self):
-        return self.user
+        return self.first_name
 
 
 class Product(models.Model):
